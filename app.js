821b2a3dc76184ba02dcf9bcf9f03264296dcb4f
Apple <tiffine.koch@gmail.com>
2016-05-23 22:07:12 -0700

    fixing image size

--- app.js
+++ app.js
@@ -20,13 +20,9 @@ mongoose.connect(mongoUrl, function(err) {
   }
 });
 
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -38,17 +34,12 @@ app.use('/ladies', require('./routes/ladies'));
 app.use('/cats', require('./routes/cats'));
 app.use('/forms', require('./routes/forms'));
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -59,8 +50,6 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -68,6 +57,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
